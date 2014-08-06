59b3fb9fc7822ba9a4bb77293c59341ca42b4ffb
MartinHjelm <mar.hjelm@gmail.com>
2014-08-06 14:22:06 +0200

    Fixed the pcd viewer to working

--- viewPCD.cpp
+++ viewPCD.cpp
@@ -1,28 +1,38 @@
-#include <pcl/visualization/cloud_viewer.h>
-#include <iostream>
-#include <pcl/io/io.h>
+#include <pcl/point_types.h>
 #include <pcl/io/pcd_io.h>
+#include <pcl/visualization/point_cloud_color_handlers.h>
 #include <pcl/visualization/pcl_visualizer.h>
       
 int 
 main(int argc, char ** argv)
 {
 
-    // Load point cloud from file
-    std::string fName = "";
-    if(argv[1]) fName = std::string(argv[1]);
-    else return 0;
-    pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGBA>);
-    pcl::io::loadPCDFile (fName, *cloud);
-    
-    pcl::visualization::CloudViewer viewer("Cloud Viewer");
-    
-    viewer.showCloud(cloud);
-    
-    while (!viewer.wasStopped ())
-    {
-      // viewer.spinOnce (100);
-      boost::this_thread::sleep (boost::posix_time::microseconds (100000));
-    }
+  typedef pcl::PointXYZRGB PointT;
+
+  // Load point cloud from file
+  std::string fName = "";
+  if(argv[1]) fName = std::string(argv[1]);
+  else return 0;
+  pcl::PointCloud<PointT>::Ptr cloud (new pcl::PointCloud<PointT>);
+  pcl::PCDReader reader;
+  reader.read (fName, *cloud);
+
+  // // View Point Cloud
+  pcl::visualization::PCLVisualizer::Ptr viewer (new pcl::visualization::PCLVisualizer ("Point Cloud from "+fName));
+  pcl::visualization::PointCloudColorHandlerRGBField<PointT> rgb;
+  rgb.setInputCloud(cloud);
+  viewer->addPointCloud<PointT> (cloud, rgb, "Cloud");
+  viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "Cloud");  
+  viewer->setBackgroundColor (0, 0, 0);
+  viewer->addCoordinateSystem (0.25, "global");
+  // viewer->initCameraParameters ();
+  // viewer->setCameraPosition(0,0,0,1,-1,1,0);
+  viewer->setCameraPosition(0, 0, -1, 0, -1, 0);
+
+  while (!viewer->wasStopped ())
+  {
+    viewer->spinOnce (100);
+    boost::this_thread::sleep (boost::posix_time::microseconds (100000));
+  }
     return 0;
 }
